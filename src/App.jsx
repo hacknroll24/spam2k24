b6604176a937f3daa3a421f98e3472d67fbf61fb
zhi wei <e0544531@u.nus.edu>
2024-01-20 22:47:04 +0800

    App jsx changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ import React from "react";
 import "./App.css";
 import "./Countdown.css";
 
-function ContestantBox({ initialCountdown, remainingTime }) {
+function ContestantBox({
+  player,
+  setPlayers,
+  // initialCountdown,
+  // remainingTime,
+}) {
   const [isClicked, setIsClicked] = useState(false);
-  const [iq, setIq] = useState(0);
-
-  const isGameStarted = remainingTime > 0 && initialCountdown === 0;
+  // const isGameStarted = remainingTime > 0 && initialCountdown === 0;
 
   const parentRef = useRef(null);
 
@@ -23,38 +26,32 @@ function ContestantBox({ initialCountdown, remainingTime }) {
     }, 10);
   }
 
-  const handleClick = () => {
-    // if (counter > 0 && counter1 === 0) {
-    if (isGameStarted) {
-      setIsClicked(true);
-      setTimeout(() => {
-        setIsClicked(false);
-      }, 10); // resets back after 0.5 seconds
-      setIq((prev) => prev + 1);
+  // const handleClick = () => {
+  // if (counter > 0 && counter1 === 0) {
+  // if (isGameStarted) {
+  //   if (true) {
+  //     setIsClicked(true);
+  //     setTimeout(() => {
+  //       setIsClicked(false);
+  //     }, 10);
 
-      animatePlusOne();
-    } else {
-      setIsClicked(false);
-    }
-  };
+  //     // socket.emit("", {
+  //     //   ...player,
+  //     //   iq: player.iq + 1
+  //     // })
+
+  //     animatePlusOne();
+  //   } else {
+  //     setIsClicked(false);
+  //   }
+  // }
 
   return (
     <div className="parent-anchor" ref={parentRef}>
-      {initialCountdown > 0 && (
-        <div className={`countdown countdown-${initialCountdown}`}>
-          {initialCountdown}
-        </div>
-      )}
-      {initialCountdown === 0 && (
-        <div className="countdown countdown-0">Go!</div>
-      )}
-      <div className="countdown30">
-        Countdown: {remainingTime === 0 ? "Time over" : remainingTime}
-      </div>
-      <div className="count">Total IQ: {iq}</div>
+      <div style={{}}>Total IQ: {player.iq}</div>
       <div
         className={`bobbingElement ${isClicked && "big"}`}
-        onClick={handleClick}
+        // onClick={handleClick}
       >
         {/* Click me! */}
         <img
@@ -66,32 +63,56 @@ function ContestantBox({ initialCountdown, remainingTime }) {
     </div>
   );
 }
-
 export default function App() {
-  // const [score, setScore] = useState({});
   const [remainingTime, setRemainingTime] = React.useState(30);
   const [initialCountdown, setInitialCountdown] = useState(3);
 
-  useEffect(() => {
-    // Check if the 3-second timer has finished
-    if (initialCountdown > 0) {
-      const timer = setTimeout(() => {
-        setInitialCountdown(initialCountdown - 1);
-      }, 1000);
-      return () => clearTimeout(timer);
-    } else {
-      // The 3-second timer has finished, start the 30-second timer
-      remainingTime > 0 &&
-        setTimeout(() => setRemainingTime(remainingTime - 1), 1000);
-    }
-  }, [initialCountdown, remainingTime]);
+  const [players, setPlayers] = useState({
+    Shiying: 0,
+    Zhiwei: 0,
+    Bryann: 0,
+    Alvin: 0,
+  });
+  console.log(Object.keys(players));
+
+  // useEffect(() => {
+  //   // Check if the 3-second timer has finished
+  //   if (initialCountdown > 0) {
+  //     const timer = setTimeout(() => {
+  //       setInitialCountdown(initialCountdown - 1);
+  //     }, 1000);
+  //     return () => clearTimeout(timer);
+  //   } else {
+  //     // The 3-second timer has finished, start the 30-second timer
+  //     remainingTime > 0 &&
+  //       setTimeout(() => setRemainingTime(remainingTime - 1), 1000);
+  //   }
+  // }, [initialCountdown, remainingTime]);
+
+  // console.log(players);
 
   return (
     <div className="grid">
-      <ContestantBox
-        remainingTime={remainingTime}
-        initialCountdown={initialCountdown}
-      />
+      {Object.keys(players).map((player) => {
+        return (
+          <ContestantBox
+            key={player}
+            player={player}
+            // remainingTime={remainingTime}
+            // initialCountdown={initialCountdown}
+          />
+        );
+      })}
+      {/* {Object.keys(players).map((player) => {
+        return (
+          <ContestantBox
+            key={player}
+            player={player}
+            // remainingTime={remainingTime}
+            // initialCountdown={initialCountdown}
+          />
+        );
+      })} */}
     </div>
   );
 }
