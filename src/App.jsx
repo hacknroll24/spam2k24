188987ce2920436782844292bd200bd6db2fd6d6
Bryann Yeap Kok Keong <bryannyeapkk@gmail.com>
2024-01-20 16:19:15 +0800

    mvp of single player

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,39 @@
 // src/App.jsx
-import { useState, useEffect } from "react";
+import { useRef } from "react";
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [data, setData] = useState(null);
+export default function App() {
+  const [isClicked, setIsClicked] = useState(false);
+  const [count, setCount] = useState(0);
+  const parentRef = useRef(null);
 
-  useEffect(() => {
-    // Make a request to your Express server
-    fetch(`http://localhost:${import.meta.env.VITE_API_PORT}/api/data`)
-      .then((response) => response.json())
-      .then((result) => {
-        setData(result);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, []);
+  const handleClick = () => {
+    setIsClicked(true);
+    setTimeout(() => {
+      setIsClicked(false);
+    }, 10); // resets back after 0.5 seconds
+    setCount((prev) => prev + 1);
+
+    // animate
+    const newDiv = document.createElement("div");
+    newDiv.textContent = "IQ + 1";
+    newDiv.classList.add("floatingText");
+    parentRef.current.appendChild(newDiv);
+    setTimeout(() => {
+      newDiv.remove();
+    }, 1000);
+  };
 
   return (
-    <div>
-      <h1>Vite React App</h1>
-      {data && <p>Data from server: {data.message}</p>}
+    <div className="parent-anchor" ref={parentRef}>
+      <div className="count">Total IQ: {count}</div>
+      <div
+        className={`bobbingElement ${isClicked && "big"}`}
+        onClick={handleClick}
+      >
+        Click me!
+      </div>
     </div>
   );
 }
-
-export default App;
