855ab8cf93b025c5aa0c82eb8455cd4b71099c5c
zhi wei <e0544531@u.nus.edu>
2024-01-21 11:46:32 +0800

    done

--- src/Congrats.jsx
+++ src/Congrats.jsx
@@ -14,13 +14,61 @@ function Congrats() {
     handleEndGame();
   };
 
+  function getLeaderBoard() {
+    const scores = Object.keys(users).map((playerName) => {
+      return { name: playerName, score: users[playerName] };
+    });
+
+    scores.sort((a, b) => b.score - a.score);
+    return scores;
+  }
+
   return (
     <div className="container">
-      <h1> Congratulations!</h1>
-      <div className="subheading">
-        <h2>To</h2>
-        <div className="winner">{winner}</div>
-        <h2> {`for a successful +${winnerIq} boost in IQ!`}</h2>
+      <div
+        style={{
+          textAlign: "center",
+        }}
+      >
+        <h1> Congratulations!</h1>
+        <div
+          className="subheading"
+          style={{
+            fontSize: "1.5rem",
+            letterSpacing: "0.3rem",
+          }}
+        >
+          <h2>To</h2>
+          <div className="winner">{winner}</div>
+          <h2> {`for a successful +${winnerIq} boost in IQ!`}</h2>
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          gap: "10px",
+          fontSize: "2rem",
+        }}
+      >
+        {getLeaderBoard().map((scoreInfo, index) => (
+          <div
+            style={{
+              display: "flex",
+            }}
+            key={scoreInfo.score + scoreInfo.name}
+          >
+            <div
+              style={{
+                marginRight: "10px",
+              }}
+            >
+              {index + 1}. {scoreInfo.name}
+            </div>
+            <div>{scoreInfo.score}</div>
+          </div>
+        ))}
       </div>
 
       <button className="button" onClick={handleOnClick}>
